4965e66d900958d523ff8d77102ebb1171ceaa41
srivathsanvenkateswaran <srivathsanvenkateswaran@gmail.com>
2022-02-19 18:26:46 +0530

    Impl 19

---
+++ implementationOrConstructive/19.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+using namespace std;
+
+int main()
+{
+    string s;
+    cin >> s;
+    vector<int> v;
+
+    for(int i=0; i<s.length(); i++)
+    {
+        if(s[i] != '+')
+        {
+            v.push_back(int(s[i]-'0'));
+        }
+    }
+
+    sort(v.begin(), v.end());
+
+    for(int i=0; i<v.size()-1; i++)
+    {
+        cout << v[i] << '+';
+    }
+
+    cout << v[v.size()-1];
+}
