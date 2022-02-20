ae65eac6dbf66b989880acf988b53d772aeb834e
srivathsanvenkateswaran <srivathsanvenkateswaran@gmail.com>
2022-02-20 13:09:51 +0530

    Impl 21

---
+++ implementationOrConstructive/21.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int arr[n];
+
+    for(int i=0; i<n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    int ans = 0;
+    int count = 1;
+
+    for(int i=1; i<n; i++)
+    {
+        if(arr[i] > arr[i-1])
+        {
+            count++;
+        }
+        else
+        {
+            ans = max(ans, count);
+            count = 1;
+        }
+    }
+
+    ans = max(ans, count);
+
+    cout << ans;
+
+    return 0;
+}
