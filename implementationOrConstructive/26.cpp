b7cddcaebda8dc4ec325ddb97a46e3762cf1cbff
srivathsanvenkateswaran <srivathsanvenkateswaran@gmail.com>
2022-02-26 18:18:24 +0530

    Impl 26

--- implementationOrConstructive/26.cpp
+++ implementationOrConstructive/26.cpp
@@ -1,2 +1,23 @@
-1
-z
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n; 
+    cin >> n;
+
+    string encryptedString;
+    cin >> encryptedString;
+
+    int increment = 1;
+    int i = 0;
+
+    while(i<n)
+    {
+        cout << encryptedString[i];
+        i += increment;
+        increment++;
+    }
+
+    return 0;
+}
